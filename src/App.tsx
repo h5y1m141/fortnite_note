aa613809f9e7fe11f7521c621c31c9afc9e759db
HiroshiOyamada <h5y1m141+github@gmail.com>
2021-05-20 10:10:13 +0900

    不要なimportの記述を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Grid, Box, ThemeProvider } from '@material-ui/core'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { theme } from './styles/theme'
 import { NewNote } from './pages/admin/notes/NewNote'
 import { SignIn } from './pages/admin/SignIn'
